7caa5c7c744d8e9b80979da0acde9361b1d70464
Jesse Buchanan <hello@jessebuchanan.ca>
2016-10-25 21:28:44 -0400

    initial pass

--- src/App.js
+++ src/App.js
@@ -1,18 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import moment from 'moment'
 import './App.css';
 
+
+const stops = [
+  { name: 'W', url: 'http://restbus.info/api/agencies/ttc/routes/85/stops/10133/predictions' },
+  { name: 'E', url: 'http://restbus.info/api/agencies/ttc/routes/85/stops/9322/predictions' },
+]
+
+// const getPrediction = async (url) => {
+//   const data = await fetch(url)
+//   return data
+// }
+
+
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      lastUpdated: moment().subtract(100000, 'days')
+    }
+  }
+
+  componentWillMount() {
+    this.updateLastUpdated()
+    this.updatePredictions()
+  }
+
+  componentDidMount() {
+    setInterval(this.updateLastUpdated.bind(this), 1000)
+    setInterval(this.updatePredictions.bind(this), 60*1000)
+  }
+
+  updateLastUpdated() {
+    console.log(this.state)
+    const lastUpdated = this.state.lastUpdated
+    this.setState({ lastUpdatedMessage:
+      lastUpdated.fromNow()
+    })
+  }
+
+  updatePredictions() {
+    stops.forEach((stop, index) => {
+      fetch(stop.url)
+        .then(response => response.json())
+        .then(json => json[0])
+        .then(data => {
+          console.log(data)
+          const prediction = data['values'].map((v) => (
+            {
+              minutes: v.minutes,
+              seconds: v.seconds,
+              vehicle_id: v.vehicle.id,
+              title: v.direction.title,
+            }
+          ))
+          this.setState({ [`prediction${index}`]: prediction })
+
+          // bug: timers aren't independent
+          this.setState({ lastUpdated: moment() })
+        })
+    })
+  }
+
+  renderPredictions(predictions) {
+    if (predictions == null) return '?'
+    const minutes = predictions.map(p => `${p.minutes}m`)
+    return minutes.join(', ')
+  }
+
+  lastUpdated() {
+    return this.state.lastUpdated.fromNow()
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>WEST: {this.renderPredictions(this.state.prediction0)}</p>
+        <p>EAST: {this.renderPredictions(this.state.prediction1)}</p>
+        <p className="small">Last updated {this.state.lastUpdatedMessage}</p>
       </div>
     );
   }
