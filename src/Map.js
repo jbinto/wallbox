86e8ea58212a155ff17f48d6f16264f06247d878
Jesse Buchanan <hello@jessebuchanan.ca>
2016-10-30 00:48:04 -0400

    google maps api key

--- src/Map.js
+++ src/Map.js
@@ -3,6 +3,8 @@ import GoogleMap from 'google-map-react'
 import Marker from './Marker'
 
 
+const GMAPS_API_KEY = 'AIzaSyAc-O4-ql107qeHBT2E_CXIW3V2GTXs5Fk'
+
 // https://snazzymaps.com/style/44/mapbox
 const MAP_STYLES = [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
 
@@ -17,6 +19,9 @@ const Map = ({ location, westPrefix, eastPrefix }) => {
   // is chopped off (sorry trademark lawyers)
   return <div style={{ height: 350, width: 238, display: "inline-block" }}>
     <GoogleMap
+      bootstrapURLKeys={{
+        key: GMAPS_API_KEY
+      }}
       center={getLatLng(location)}
       zoom={16}
       options={{
